79e1fda16b742e1e7bd30b1afc3942438e2acbac
IrfanAzam <iefan2699@gmail.com>
2023-03-24 12:35:28 +0800

    sec commit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,41 +19,41 @@ class UserSeeder extends Seeder
             'email'     => 'user@user.com',
             'password'  =>  bcrypt('password'),
             'staff_no'  => '1122',
-            'phone_no'  => '0173803902',
+            'phone_no'  => '0173422222',
         ]);
 
         User::create([
             'id'        => '2',
-            'name'      => 'SYED ZULKARNAEN BIN HASSAN',
-            'email'     => 'syed@mbsj.gov.my',
+            'name'      => 'SYED',
+            'email'     => 'syed@mbsj.my',
             'password'  =>  bcrypt('pass4371'),
             'staff_no'  => '2222',
-            'phone_no'  => '0162559587',
+            'phone_no'  => '0163291023',
         ]);
         User::create([
             'id'        => '3',
-            'name'      => 'NURAZLINA BT KAMARUDIN',
-            'email'     => 'nurazlina@mbsj.gov.my',
+            'name'      => 'NURAZLINA',
+            'email'     => 'nurazlina@mbsj.my',
             'password'  =>  bcrypt('pass4371'),
             'staff_no'  => '1122',
-            'phone_no'  => '0193360095',
+            'phone_no'  => '0193312201',
         ]);
         User::create([
             'id'        => '4',
-            'name'      => 'MOHD NAZLY BIN ISMAIL',
-            'email'     => 'nazly@mbsj.gov.my',
+            'name'      => 'MOHD NAZLY',
+            'email'     => 'nazly@mbsj.my',
             'password'  =>  bcrypt('pass4371'),
             'staff_no'  => '1122',
-            'phone_no'  => '0173803902',
+            'phone_no'  => '0173002001',
         ]);
 
         User::create([
             'id'        => '5',
-            'name'      => 'MOHD RAZUHRIN BIN RAHMAD',
-            'email'     => 'razuhrin@mbsj.gov.my',
+            'name'      => 'MOHD RAZUHRIN',
+            'email'     => 'razuhrin@mbsj.my',
             'password'  =>  bcrypt('pass4371'),
             'staff_no'  => '1111',
-            'phone_no'  => '0192622944',
+            'phone_no'  => '0192021010',
         ]);
        
     }
